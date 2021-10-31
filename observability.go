ec2c9f7b9d93bc5379631fb0c94c93453e9dc40f
Dmitry Harupa <dmitry.harupa@egt-gp.com>
2021-10-31 14:32:14 +0200

    observability: NewTeleObservability constructor. This flow uses https://github.com/cloudevents/sdk-go/tree/main/observability/opentelemetry/v2 approach
    * option WithSpanNameFormatter - you can decide how to manage span name flow
    * WithSpanAttributesGetter - allow insert more attributes
    
    Signed-off-by: Dmitry Harupa <dmitry.harupa@egt-gp.com>

--- observability.go
+++ observability.go
@@ -2,17 +2,27 @@ package protonats
 
 import (
 	"context"
-	"fmt"
+	"runtime"
+	"strings"
 	"time"
 
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/binding"
+	"github.com/cloudevents/sdk-go/v2/client"
 	"github.com/cloudevents/sdk-go/v2/event"
+	"github.com/cloudevents/sdk-go/v2/observability"
+	"github.com/d7561985/protonats/adapter"
 	"github.com/d7561985/tel"
 	"github.com/d7561985/tel/monitoring/metrics"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"go.uber.org/zap"
 )
+
+const componentName = "cloud.events.protocol.nats.observability"
+
+type SpanNameFormatter func(cloudevents.Event) string
+type SpanAttrGetter func(cloudevents.Event) opentracing.Tags
 
 // TeleObservability implement cloudevents client.ObservabilityService with OpenTracing propagation
 // This flow idempotent and not tight coupled to NATS
@@ -21,22 +31,36 @@ import (
 // Producer component handled in RecordSendingEvent and has specific context requirements
 type TeleObservability struct {
 	*tel.Telemetry
+
 	Metrics metrics.MetricsReader
+
+	spanAttributesGetter SpanAttrGetter
+	spanNameFormatter    SpanNameFormatter
+}
+
+func NewTeleObservability(t *tel.Telemetry, m metrics.MetricsReader, opts ...ObservabilityOption) client.ObservabilityService {
+	res := &TeleObservability{
+		Telemetry:            t,
+		Metrics:              m,
+		spanAttributesGetter: nil,
+		spanNameFormatter:    defaultSpanNameFormatter,
+	}
+
+	for _, fn := range opts {
+		fn(res)
+	}
+
+	return res
 }
 
 // RecordSendingEvent producer interceptor required context argument to be polluted with tel context
 // creates new tracing brunch from provided inside context OpenTracing or tel data
 func (t *TeleObservability) RecordSendingEvent(_ctx context.Context, e event.Event) (context.Context, func(errOrResult error)) {
-	span, ctx := tel.StartSpanFromContext(_ctx, fmt.Sprintf("SEND EVENT: %s", e.Type()))
+	attr := t.GetSpanAttributes(e, getFuncName())
+	span, ctx := tel.StartSpanFromContext(_ctx, t.getSpanName(&e, "send"), attr)
 
-	ext.Component.Set(span, "cloud.events.protocol.nats.observability")
+	ext.Component.Set(span, componentName)
 	ext.SpanKindProducer.Set(span)
-
-	span.PutFields(
-		zap.String("cloud.event.type", e.Type()),
-		zap.String("key", e.ID()),
-		zap.String("info", e.String()),
-	)
 
 	cb := func(err error) {
 		defer span.Finish()
@@ -45,12 +69,10 @@ func (t *TeleObservability) RecordSendingEvent(_ctx context.Context, e event.Eve
 	return ctx, cb
 }
 
+// InboundContextDecorators returns a decorator function that allows enriching the context with the incoming parent trace.
+// This method gets invoked automatically by passing the option 'WithObservabilityService' when creating the cloudevents client.
 func (t *TeleObservability) InboundContextDecorators() []func(context.Context, binding.Message) context.Context {
-	return []func(context.Context, binding.Message) context.Context{}
-}
-
-func (t TeleObservability) RecordReceivedMalformedEvent(_ context.Context, _ error) {
-	panic("implement me")
+	return []func(context.Context, binding.Message) context.Context{t.tracePropagatorContextDecorator}
 }
 
 // RecordCallingInvoker consumer middleware
@@ -59,12 +81,8 @@ func (t TeleObservability) RecordReceivedMalformedEvent(_ context.Context, _ err
 // consumer represent invoker model for opentracing,
 //from tracing objectives this mean that it begin new span either and that span should be return and used by others
 func (t *TeleObservability) RecordCallingInvoker(_ctx context.Context, e *event.Event) (context.Context, func(errOrResult error)) {
-	opt := make([]opentracing.StartSpanOption, 0, 4)
-	opt = append(opt, opentracing.Tags{
-		"cloud.event.type": e.Type(),
-		"key":              e.ID(),
-		"event info":       e.String(),
-	})
+	opt := make([]opentracing.StartSpanOption, 0, 2)
+	opt = append(opt, t.GetSpanAttributes(*e, getFuncName()))
 
 	if ref := _ctx.Value(opentracing.SpanReference{}); ref != nil {
 		if v, ok := ref.(opentracing.SpanReference); ok {
@@ -73,23 +91,17 @@ func (t *TeleObservability) RecordCallingInvoker(_ctx context.Context, e *event.
 	}
 
 	tr, start := t.Copy(), time.Now()
-	span, ctx := tr.StartSpan(fmt.Sprintf("GET EVENT: %s", e.Type()), opt...)
+	span, ctx := tr.StartSpan(t.getSpanName(e, "process"), opt...)
 
-	ext.Component.Set(span, "cloud.events.protocol.nats.observability")
+	ext.Component.Set(span, componentName)
 	ext.SpanKindConsumer.Set(span)
 	tel.UpdateTraceFields(ctx)
-
-	span.PutFields(
-		zap.String("cloud.event.type", e.Type()),
-		zap.String("key", e.ID()),
-		zap.String("info", e.String()),
-	)
 
 	cb := func(err error) {
 		defer span.Finish()
 
 		t.Metrics.AddReaderTopicHandlingTime(e.Type(), time.Since(start))
-		span.PutFields(zap.Duration("duration", time.Since(start)))
+		span.PutFields(zap.String("duration", time.Since(start).String()))
 
 		if err != nil {
 			t.Metrics.AddReaderTopicFatalError(e.Type(), 1)
@@ -108,6 +120,96 @@ func (t *TeleObservability) RecordCallingInvoker(_ctx context.Context, e *event.
 	return ctx, cb
 }
 
+// RecordReceivedMalformedEvent if content is unpredictable
+func (t *TeleObservability) RecordReceivedMalformedEvent(ctx context.Context, err error) {
+	spanName := observability.ClientSpanName + ".malformed receive"
+	span, _ := tel.FromCtx(ctx).StartSpan(spanName)
+	defer span.Finish()
+
+	ext.Component.Set(span, componentName)
+	ext.SpanKindConsumer.Set(span)
+
+	span.Error(spanName, zap.Error(err))
+}
+
 func (t TeleObservability) RecordRequestEvent(ctx context.Context, _ event.Event) (context.Context, func(error, *event.Event)) {
 	return ctx, func(errOrResult error, e *event.Event) {}
+}
+
+// getSpanName Returns the name of the span.
+//
+// When no spanNameFormatter is present in OTelObservabilityService,
+// the default name will be "cloudevents.client.<eventtype> prefix" e.g. cloudevents.client.get.customers send.
+//
+// The prefix is always added at the end of the span name. This follows the semantic conventions for
+// messasing systems as defined in https://github.com/open-telemetry/opentelemetry-specification/blob/v1.6.1/specification/trace/semantic_conventions/messaging.md#operation-names
+func (t TeleObservability) getSpanName(e *cloudevents.Event, suffix string) string {
+	name := t.spanNameFormatter(*e)
+
+	// make sure the span name ends with the suffix from the semantic conventions (receive, send, process)
+	if !strings.HasSuffix(name, suffix) {
+		return name + " " + suffix
+	}
+
+	return name
+}
+
+// Extracts the traceparent from the msg and enriches the context to enable propagation
+func (t *TeleObservability) tracePropagatorContextDecorator(ctx context.Context, msg binding.Message) context.Context {
+	v, ok := msg.(*msgErr)
+	if !ok {
+		return ctx
+	}
+
+	spanCtx, err := t.T().Extract(opentracing.TextMap, adapter.NewHeader(&v.Message.Msg.Header))
+	if err != nil {
+		return ctx
+	}
+
+	return context.WithValue(t.Ctx(), opentracing.SpanReference{}, opentracing.ChildOf(spanCtx))
+}
+
+func getFuncName() string {
+	pc := make([]uintptr, 1)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+
+	// frame.Function should be github.com/cloudevents/sdk-go/observability/opentelemetry/v2/client.OTelObservabilityService.Func
+	parts := strings.Split(frame.Function, ".")
+
+	// we are interested in the function name
+	if len(parts) != 4 {
+		return ""
+	}
+	return parts[3]
+}
+
+// GetSpanAttributes returns the attributes that are always added to the spans
+func (t *TeleObservability) GetSpanAttributes(e cloudevents.Event, method string) opentracing.Tags {
+	attr := opentracing.Tags{
+		"code.function":               method,
+		observability.SpecversionAttr: e.SpecVersion(),
+		observability.IdAttr:          e.ID(),
+		observability.TypeAttr:        e.Type(),
+		observability.SourceAttr:      e.Source(),
+	}
+	if sub := e.Subject(); sub != "" {
+		attr[observability.SubjectAttr] = sub
+	}
+	if dct := e.DataContentType(); dct != "" {
+		attr[observability.DatacontenttypeAttr] = dct
+	}
+	if t.spanAttributesGetter != nil {
+		a := t.spanAttributesGetter(e)
+		for k, v := range a {
+			attr[k] = v
+		}
+	}
+
+	return attr
+}
+
+var defaultSpanNameFormatter = func(e cloudevents.Event) string {
+	return observability.ClientSpanName + "." + e.Context.GetType()
 }
--- protocol_test.go
+++ protocol_test.go
@@ -1,4 +1,7 @@
-package protonats
+//go:build internal_tests
+// +build internal_tests
+
+package protonats_test
 
 import (
 	"context"
@@ -9,6 +12,7 @@ import (
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/client"
 	cecontext "github.com/cloudevents/sdk-go/v2/context"
+	"github.com/d7561985/protonats"
 	"github.com/d7561985/tel"
 	"github.com/d7561985/tel/monitoring/metrics"
 	"github.com/google/uuid"
@@ -30,10 +34,10 @@ const (
 )
 
 func TestA(t *testing.T) {
-	p, err := NewProtocol("127.0.0.1:4222", "xxxx", TopicA,
+	p, err := protonats.NewProtocol("127.0.0.1:4222", "", TopicA,
 		[]nats.Option{},
-		WithConsumerOptions(
-			WithQueuePoolSubscriber("api-service",
+		protonats.WithConsumerOptions(
+			protonats.WithQueuePoolSubscriber("api-service",
 				TopicB, TopicA,
 			),
 		),
@@ -51,11 +55,9 @@ func TestA(t *testing.T) {
 	ctx := tl.Ctx()
 
 	ce, err := cloudevents.NewClient(p,
-		client.WithObservabilityService(&TeleObservability{
-			Metrics:   metrics.NewCollectorMetricsReader(),
-			Telemetry: &tl,
-		}))
-
+		client.WithObservabilityService(
+			protonats.NewTeleObservability(&tl, metrics.NewCollectorMetricsReader())),
+	)
 	require.NoError(t, err)
 
 	done := make(chan struct{})
@@ -89,6 +91,7 @@ func TestA(t *testing.T) {
 		Message:  "Hello World",
 	})
 
+	// inject topic
 	ctx = cecontext.WithTopic(ctx, TopicB)
 
 	err = ce.Send(ctx, e)
